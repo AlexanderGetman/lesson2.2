519a31d798bad1efdc0c201627bf0ec4af5fe8bb
AlexanderGetman <40514260+AlexanderGetman@users.noreply.github.com>
2018-07-23 22:10:42 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,8 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-$filename = __DIR__.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR.$_GET["test"].'.json';
+$filename = __DIR__.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR.$_GET["test"];
 $json = file_get_contents($filename);
 $contents = json_decode($json, true);
-//print_r($json);
-$counter = 1;
-$inputName = 'q';
 
 ?>
 
@@ -22,7 +19,7 @@ $inputName = 'q';
 <?php foreach ($contents as $item) :?>
     <fieldset>
         <legend><?php echo $item['question']?></legend>
-        <?php foreach ($item['answers'] as $variant) :?><label><input type="radio" name="<?php echo $inputName.$counter++?>"><?php echo $variant;?></label><?php endforeach;?>
+        <?php foreach ($item['answers'] as $variant) :?><label><input type="radio" name="<?php echo $item['number']?>"><?php echo $variant;?></label><?php endforeach;?>
     </fieldset>
 <?php endforeach;?>
 </form>
