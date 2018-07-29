94567f19ec1b2dd19100d61649e1761ab717ac4b
AlexanderGetman <40514260+AlexanderGetman@users.noreply.github.com>
2018-07-29 15:18:57 +1000

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,21 @@ header('Content-Type: text/html; charset=utf-8');
 $filename = __DIR__.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR.$_GET["test"];
 $json = file_get_contents($filename);
 $contents = json_decode($json, true);
+$tests = $_GET["test"];
+
+$correct = [];
+foreach ($contents as $item)
+{
+    $correct[]=$item['correct'] ;
+}
+
+
+if ($_POST == $correct)
+{
+    echo 'Вы правильно ответили на вопросы теста';
+} else {
+    echo 'Попытайтесь ещё раз';
+}
 
 ?>
 
@@ -15,14 +30,16 @@ $contents = json_decode($json, true);
 </head>
 <body>
 <h1>Тест</h1>
-<form action="" method="post">
+<form action=<?php echo '"test.php?test='.$tests.'"'?> method="POST">
 <?php foreach ($contents as $item) :?>
     <fieldset>
         <legend><?php echo $item['question']?></legend>
-        <?php foreach ($item['answers'] as $variant) :?><label><input type="radio" name="<?php echo $item['number']?>"><?php echo $variant;?></label><?php endforeach;?>
+        <?php foreach ($item['answers'] as $variant) :?><label><input type="radio" value="<?php echo $variant;?>" name="<?php echo $item['number']?>"><?php echo $variant;?></label><?php endforeach;?>
     </fieldset>
 <?php endforeach;?>
-</form>
 <input type="submit" placeholder="Отправить"/>
+</form>
 </body>
 </html>
+
+<?php
