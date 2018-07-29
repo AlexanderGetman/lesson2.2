23d457287787eb87926aa619e30f8ceece3c4632
AlexanderGetman <40514260+AlexanderGetman@users.noreply.github.com>
2018-07-29 15:18:44 +1000

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,45 +1,27 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-$filename = __DIR__.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR.$_GET["test"];
-$json = file_get_contents($filename);
-$contents = json_decode($json, true);
-$tests = $_GET["test"];
-
-$correct = [];
-foreach ($contents as $item)
-{
-    $correct[]=$item['correct'] ;
+if(isset($_POST['submit'])){
+    header('Location: list.php');
 }
-
-
-if ($_POST == $correct)
-{
-    echo 'Вы правильно ответили на вопросы теста';
+$dest = __DIR__.'/test'.'/test'.$_FILES['test']['name'];
+if (move_uploaded_file($_FILES['test']['tmp_name'], $dest)) {
+    echo "Файл загружен";
 } else {
-    echo 'Попытайтесь ещё раз';
+    echo "Файл не загружен";
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Тест</title>
+    <title>Домашнее задание</title>
 </head>
 <body>
-<h1>Тест</h1>
-<form action=<?php echo '"test.php?test='.$tests.'"'?> method="POST">
-<?php foreach ($contents as $item) :?>
-    <fieldset>
-        <legend><?php echo $item['question']?></legend>
-        <?php foreach ($item['answers'] as $variant) :?><label><input type="radio" value="<?php echo $variant;?>" name="<?php echo $item['number']?>"><?php echo $variant;?></label><?php endforeach;?>
-    </fieldset>
-<?php endforeach;?>
-<input type="submit" placeholder="Отправить"/>
+<form method="post" enctype="multipart/form-data">
+    Отправить: <input name="test" type="file">
+    </br>
+    <input type="submit" name="submit" value="Отправить файл">
 </form>
+    <p>Перейти на страницу с <a href="list.php">тестами</a></p>
 </body>
 </html>
-
-<?php
